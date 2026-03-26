0b343fd4e5a56d31d20b8865e0cad42bf85ed4ba
splixcube <splixcube@gmail.com>
2026-03-26 12:42:26 +0530

    removed fb

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.justmove.supplement',
   appName: 'Just Move Supplement',
   webDir: 'www',
-  // server: {
-  //   url: 'https://justmove-2eb09.web.app/',
-  //   cleartext: true,
-  // },
+ /*   server: {
+     url: 'https://justmove-2eb09.web.app/',
+    cleartext: true
+   }, */
   plugins: {
     SplashScreen: {
       // how long to show the launch splash (ms)
---
+++ readme.md
@@ -0,0 +1,4 @@
+package name -  com.justmove.supplement
+jks file name - key0
+password -      justmove
+first and last name - Just Move Supplements
